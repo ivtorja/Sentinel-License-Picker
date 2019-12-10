f808234dfb7b3c12fb09387a1ae54a46e93616e5
Iván Torres <ivtorja@gmail.com>
2019-12-10 09:24:31 +0100

    INIT parser added. The program now opens NX.

--- Sentinel-license-picker.py
+++ Sentinel-license-picker.py
@@ -4,15 +4,23 @@ import re
 import shutil
 import os
 import ctypes
+from configparser import ConfigParser
 
 # URL del Sentinel
 url = 'http://localhost:6002/keyinfo.xml'
 
 # Path del archivo init
-default_ini_path = "C:\\nx_license_checker.ini"
+default_ini_path = r"C:\nx_license_checker.ini"
+
+# Parseamos el archivo init
+config = ConfigParser()
+config.read(default_ini_path)
 
 # Path de las licencias
-user_input = "C:\\NX\\NX Lic"
+license_path = r"C:\NX\NX Lic"
+
+# Path de NX
+nx_path = r"C:\Siemens\NX\NXBIN\ugraf.exe"
 
 # Cargar el archivo XML en memoria
 uh = urllib.request.urlopen(url)
@@ -30,39 +38,39 @@ for each in counts:
     string = each.text
     licencia = re.sub(r"\b0{3}","",string)
 
-# Como cada usuario tiene las licencias en una carpeta distinta, 
-# creamos o buscamos un archivo de inicialización con esa ruta    
-
+# Cargamos las rutas del archivo init 
 if os.path.exists(default_ini_path):
-  init = open(default_ini_path,"r")
-  # Tomamos la ruta como variable
-  user_input = init.read()
-  init.close()
-  print(user_input)
+  license_path = config.get('paths', 'licensepath')
+  nx_path = config.get('paths', 'nxpath')
+  print(license_path)
+  print(nx_path)
 else:
     ctypes.windll.user32.MessageBoxW(0, "No encuentro el archivo de inicialización :(", "Errorcito", 1)
 
 # Borramos el archivo "licencia.txt" si existe
-if os.path.exists(user_input + "\\licencia.txt"):
-  os.remove(user_input + "\\licencia.txt")
+if os.path.exists(license_path + "\\licencia.txt"):
+  os.remove(license_path + "\\licencia.txt")
   print("Borrao")
 else:
   print("The file does not exist")
 
 # Buscamos dentro de la carpeta especificada la licencia que queremos activar
-directory = os.listdir(user_input)
+directory = os.listdir(license_path)
 searchstring = licencia
 
 for fname in directory:
-    if os.path.isfile(user_input + os.sep + fname):
+    if os.path.isfile(license_path + os.sep + fname):
         # Full path
-        f = open(user_input + os.sep + fname, 'r')
+        f = open(license_path + os.sep + fname, 'r')
 
 # Y la copiamos como "licencia.txt"
         if searchstring in f.read():
             print('%s' % fname)
-            newPath = shutil.copy(user_input + '\\%s' % fname, user_input + "\\licencia.txt")
+            newPath = shutil.copy(license_path + '\\%s' % fname, license_path + "\\licencia.txt")
 
 # Cerramos los archivos abiertos
 f.close()
+
+# Ejecutamos NX
+os.startfile(nx_path)
         
